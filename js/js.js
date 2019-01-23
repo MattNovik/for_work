c65c06d449d2b8a7728bfdda62d7886912038318
GOdick <matvej.novik@gmail.com>
2019-01-23 23:06:22 +0300

    now full json. Creating html through js

--- js/js.js
+++ js/js.js
@@ -1,57 +1,32 @@
+var local_data = data;
+   window.onload = function () {
 
-var header = document.querySelector('header');
-var section = document.querySelector('section');
+     var productPage = document.createElement('div');
+     var productLeftPage = document.createElement('div');
+        var productLogo = document.createElement('div');
+        var imgLogo = document.createElement('img');
+     var productMiddlePage = document.createElement('div');
+       var middleTop = document.createElement('div');
+          var middleTopLeft = document.createElement('div');
+            var leftText = document.createElement('span');
+            var leftTextCode = document.createElement('span');
+          var middleTopRight = document.createElement('div');  
+            var rightTextInOut = document.createElement('span');
+        var middleMiddleName = document.createElement('div');
+        var middleBottomNeeded = document.createElement('div');
+     var productRightPage = document.createElement('div');
+     var cre = document.body;
 
-var requestURL = "https://github.com/MattNovik/for_work/blob/master/products.json";
-var request = new XMLHttpRequest();
-request.withCredentials = true;
-request.open('GET', requestURL);
-request.responseType = 'json';
-request.send();
-request.onload = function() {
-  var superHeroes = request.response;
-  populateHeader(superHeroes);
-  showHeroes(superHeroes);
-}
-function populateHeader(jsonObj) {
-  var myH1 = document.createElement('h1');
-  myH1.textContent = jsonObj['squadName'];
-  header.appendChild(myH1);
+     
+     productLeftPage.innerHTML = local_data[0].title;
+     productMiddlePage.innerHTML = local_data[0].unit;
+     productRightPage.innerHTML = local_data[0].priceRetail;
 
-  var myPara = document.createElement('p');
-  myPara.textContent = 'Hometown: ' + jsonObj['homeTown'] + ' // Formed: ' + jsonObj['formed'];
-  header.appendChild(myPara);
-}
+     cre.appendChild(productPage);
+     productPage.appendChild(productLeftPage);
+     productPage.appendChild(productMiddlePage);
+     productPage.appendChild(productRightPage);
 
-function showHeroes(jsonObj) {
-  var heroes = jsonObj['members'];
-      
-  for (var i = 0; i < heroes.length; i++) {
-    var myArticle = document.createElement('article');
-    var myH2 = document.createElement('h2');
-    var myPara1 = document.createElement('p');
-    var myPara2 = document.createElement('p');
-    var myPara3 = document.createElement('p');
-    var myList = document.createElement('ul');
 
-    myH2.textContent = heroes[i].name;
-    myPara1.textContent = 'Secret identity: ' + heroes[i].secretIdentity;
-    myPara2.textContent = 'Age: ' + heroes[i].age;
-    myPara3.textContent = 'Superpowers:';
-        
-    var superPowers = heroes[i].powers;
-    for (var j = 0; j < superPowers.length; j++) {
-      var listItem = document.createElement('li');
-      listItem.textContent = superPowers[j];
-      myList.appendChild(listItem);
-    }
-
-    myArticle.appendChild(myH2);
-    myArticle.appendChild(myPara1);
-    myArticle.appendChild(myPara2);
-    myArticle.appendChild(myPara3);
-    myArticle.appendChild(myList);
-
-    section.appendChild(myArticle);
-  }
-}
+     alert("finish");
+ };
