d3491dab5ee758174dcdefbeb881f0cf58c9671b
GOdick <matvej.novik@gmail.com>
2019-01-23 19:53:54 +0300

    chenged products

--- js/js.js
+++ js/js.js
@@ -1,24 +1,10 @@
-var useActiveX = typeof ActiveXObject !== 'undefined';
-function loadJSON(file, callback) {
-    var xobj;
-    if (useActiveX) {
-        xobj = new ActiveXObject('Microsoft.XMLHTTP'); 
-    } else {
-        xobj = new XMLHttpRequest();
-    }
-    xobj.callback = callback;
-    if (xobj.overrideMimeType) {
-        xobj.overrideMimeType('application/json');
-    }
-    xobj.open('GET', file, false);
-    xobj.onreadystatechange = function() {
-        if (this.readyState === 4) {
-            this.callback(this);
-        }
-     }
-     xobj.send(null);
+var requestURL = "https://github.com/MattNovik/for_work/blob/master/products.json";
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+request.onload = function() {
+  var superHeroes = request.response;
+  populateHeader(superHeroes);
+  showHeroes(superHeroes);
 }
-
-loadJSON('products.json', function(data) {
-    alert(data);
-});
