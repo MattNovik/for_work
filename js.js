f1c118ee3dc1a45150f9de4840ea54c6d01ce514
GOdick <matvej.novik@gmail.com>
2019-01-25 21:33:16 +0300

    finished

--- js.js
+++
@@ -1,9 +0,0 @@
-var jqxhr = $.getJSON( "example.json", function() {
-console.log( "success" );
-})
-.done(function() { console.log( "second success" ); })
-.fail(function() { console.log( "error" ); })
-.always(function() { console.log( "complete" ); });
-// тут другой код...
-// другой метод, который запустится после завершения запроса
-jqxhr.complete(function() { console.log( "second complete" );
